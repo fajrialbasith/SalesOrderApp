b610f63a88213ac92f222be025bbb9a0cd6d6468
Muhammad Fajri Al Basith <fajrialbasith@gmail.com>
2024-10-30 23:15:07 +0700

    Web App

---
+++ Models/Item.cs
@@ -0,0 +1,20 @@
+
+       
+
+        using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesOrderApp.Models
+{
+    [Table("SO_ITEM")]
+    public class Item
+    {
+        [Key]
+        public int SO_ITEM_ID { get; set; }
+        public int SO_ORDER_ID { get; set; }
+        public string ITEM_NAME { get; set; }
+        public int QUANTITY { get; set; }
+        public float Price { get; set; }
+    }
+}
